2e17f254d686239825ccef50412ab1703cea1e19
Makhi7 <mvmangxola22@gmail.com>
2022-04-20 17:54:33 +0200

    Part 1 of tut

---
+++ src/main/java/com/jsonparsing/Json.java
@@ -0,0 +1,27 @@
+package com.jsonparsing;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Json {
+//    class to process everything
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    private static ObjectMapper getDefObjectMapper(){
+        ObjectMapper defObjectMapper = new ObjectMapper();
+        // ---
+        return defObjectMapper;
+    }
+
+    public static JsonNode parse(String src) throws JsonProcessingException {
+
+        return objectMapper.readTree(src);
+    }
+
+    public static <A> A fromJson(JsonNode node, Class<A> clazz) throws JsonProcessingException {
+
+        return objectMapper.treeToValue(node, clazz);
+    }
+}
---
+++ src/test/java/com/jsonparsing/JsonTest.java
@@ -0,0 +1,28 @@
+package com.jsonparsing;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.jsonparsing.pojo.SimpleTestCaseJsonPojo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonTest {
+
+    private String simpleTestCaseJsonSource = "{ \"title\":\"Json Code Grind\"} :";
+
+    @org.junit.jupiter.api.Test
+    void parse() throws JsonProcessingException {
+        JsonNode node = Json.parse(simpleTestCaseJsonSource);
+        assertEquals(node.get("title").asText(),"Json Code Grind");
+    }
+
+    @Test
+    void fromJson() throws JsonProcessingException {
+
+        JsonNode node = Json.parse(simpleTestCaseJsonSource);
+        SimpleTestCaseJsonPojo pojo = Json.fromJson(node, SimpleTestCaseJsonPojo.class);
+        assertEquals(pojo.getTitle(), "Json Code Grind");
+
+    }
+}
